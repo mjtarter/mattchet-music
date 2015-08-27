80248f72980a1d5b18cf2f2e16de7460938a8095
Matt Tarter <mjtarter@openmindwebs.com>
2015-08-27 14:49:44 -0600

    Created songs table

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150825201520) do
+ActiveRecord::Schema.define(version: 20150827204531) do
 
   create_table "artists", force: :cascade do |t|
     t.string   "artist",     limit: 255
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
+
+  create_table "songs", force: :cascade do |t|
+    t.integer  "artist_id",  limit: 4
+    t.string   "title",      limit: 255
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
   end
