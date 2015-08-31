9c0d75123dfc19732518a9f7f5c492efdbe938df
Matt Tarter <mjtarter@openmindwebs.com>
2015-08-30 18:58:09 -0600

    Added 'Add new song' feature

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -3,4 +3,10 @@ class ArtistsController < ApplicationController
 	def index
 		@songs = Song.all
 	end
+
+	def add_song
+		@song = Song.new(params.require(:song).permit(:title))
+		@song.save
+		redirect_to(:action => 'index')
+	end
 end
