9a121a5c630b1d83b12ba7366c37bccc994f2f17
Matt Tarter <mjtarter@openmindwebs.com>
2015-08-31 18:16:27 -0600

    Created 'Add Song' feature

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -5,8 +5,28 @@ class ArtistsController < ApplicationController
 	end
 
 	def add_song
-		@song = Song.new(params.require(:song).permit(:title))
+		
+		# if artist does not currently exist in DB: Add new artist to ArtistTbl & Add new title and new artist_id to SongTbl
+		# else: Add new title, and existing artist_id to Song Table
+
+		new_artist = params[:artist][:artist]
+		artist_exists = Artist.all.find_by_artist(new_artist)
+		
+		if artist_exists == nil
+			@artist = Artist.new(params.require(:artist).permit(:artist))
+			@artist.save
+
+			@song = Song.new(params.require(:song).permit(:title))
+			@song.artist_id = @artist.id
+		
+		else
+			@song = Song.new(params.require(:song).permit(:title))
+			@song.artist_id = artist_exists.id 
+		end
+		
 		@song.save
 		redirect_to(:action => 'index')
+
 	end
+
 end
