935626bea4efdc7f9ce80bf5c9a1b1e96929c54b
Matt Tarter <mjtarter@openmindwebs.com>
2015-09-25 12:47:45 -0600

    Added playlists update name functionality

--- app/controllers/playlists_controller.rb
+++ app/controllers/playlists_controller.rb
@@ -43,9 +43,25 @@ class PlaylistsController < ApplicationController
 	end
 
 	def edit
+		@playlist_id = (params[:playlist_id]) 
 		@playlist = Playlist.find(params[:playlist_id])
-		@songs = Song.all
+		@song_ids = Playlisting.where(playlist_id: @playlist_id).pluck(:song_id)
+		@songs = Song.where(id: @song_ids)
 		render('index')
+	end
+
+	def update
+		@playlist_id = params[:playlist_id]
+		@playlist = Playlist.find(params[:playlist_id])
+		@new_name = Playlist.find_by_playlist(params[:playlist][:playlist]) unless params[:playlist][:playlist].nil?
+		# If artist from form parameters does not exist in db, create new artist
+		if @new_name == nil
+			@playlist.update_attributes(playlist_params)
+			flash[:notice] = '<span class="glyphicon glyphicon-music"></span> Playlist name updated!'
+			redirect_to(:action => 'show', :playlist_id => @playlist_id)
+		else
+			render(:action => 'show', :playlist_id => @playlist_id)
+		end
 	end
 
 	private
