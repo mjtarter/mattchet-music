57cb6698273f94d862ee24f580189f5030bd7dd4
Matt Tarter <mjtarter@openmindwebs.com>
2015-10-01 11:29:32 -0600

    Added validations for playlistings / Added form for adding songs to playlists

--- app/controllers/playlists_controller.rb
+++ app/controllers/playlists_controller.rb
@@ -20,17 +20,31 @@ class PlaylistsController < ApplicationController
 	end
 
 	def create_songs
-		@playlist_id = (params[:id])
-		@song_ids = (params[:song][:song_id])
+		if (params[:id]).nil? == false
+			@playlist_id = (params[:id])
+			@song_ids = (params[:song][:song_id])
 
-		if 	@song_ids.each do |song_id|
-				Playlisting.create(:song_id => song_id, :playlist_id => @playlist_id)
+			if 	@song_ids.each do |song_id|
+					Playlisting.create(:song_id => song_id, :playlist_id => @playlist_id)
+				end
+				flash[:notice] = '<span class="glyphicon glyphicon-music"></span> Songs added to playlist!'
+				redirect_to(:action => 'show', :playlist_id => @playlist_id)
+			else
+				@songs = Song.all
+				render('index')
 			end
-			flash[:notice] = '<span class="glyphicon glyphicon-music"></span> Songs added to playlist!'
-			redirect_to(:action => 'show', :playlist_id => @playlist_id)
 		else
-			@songs = Song.all
-			render('index')
+			@playlist_id = (params[:playlist][:id])
+			@song_id = (params[:song_id])
+			@playlist = Playlisting.new(:song_id => @song_id, :playlist_id => @playlist_id)
+
+			if 	@playlist.save
+				flash[:notice] = '<span class="glyphicon glyphicon-music"></span> Songs added to playlist!'
+				redirect_to(:action => 'show', :playlist_id => @playlist_id)
+			else
+				@songs = Song.all
+				render('index')
+			end
 		end
 	end
 
--- app/models/playlisting.rb
+++ app/models/playlisting.rb
@@ -1,4 +1,6 @@
 class Playlisting < ActiveRecord::Base
 	belongs_to :playlist
 	belongs_to :song
+
+	validates_uniqueness_of :song_id, :scope => [:song_id, :playlist_id]
 end
